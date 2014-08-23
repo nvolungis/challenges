0652314ab14031ae86861de3e24948e5c325a98c
Neil Volungis <nvolungis@Neil-MacBookPro.(none)>
2014-08-23 14:43:52 -0400

    vastly simplified max continuous sum problem

--- max_consecutive_sum.js
+++ max_consecutive_sum.js
@@ -2,40 +2,27 @@
 
 (function(){
   function max_consecutive_sum(arr){
-    var si=0, ei=0, msi=0, mei=0, i=0, len = arr.length, cur_sum = 0, max_sum = 0, result = [];
+    var i=0, cur_sum = 0, max_sum = 0, len = arr.length; 
 
     for(i=0; i<len; i++){
-
       if(i === 0){
         cur_sum = max_sum = arr[i]; 
         continue;
       }
 
-      ei = i;
-
       if(cur_sum + arr[i] < arr[i]){
-        si = i;
         cur_sum = arr[i];
+
       }else{
         cur_sum += arr[i];
       } 
 
       if(cur_sum > max_sum){
-        msi = si;
-        mei = ei;
         max_sum = cur_sum;
       }
     }
 
-    if(msi === mei){
-      result.push(arr[msi]);
-    }else{
-      for(i=msi; i<=mei; i++){
-        result.push(arr[i]);       
-      }
-    }
-
-    return result;
+    return max_sum;
   }
   
   module.exports = max_consecutive_sum;;
--- test/max_consecutive_sum.js
+++ test/max_consecutive_sum.js
@@ -5,18 +5,18 @@ var assert  = require('assert'),
 describe('Max Consecutive Sum', function(){
   it('return the max sum of consecutive items', function(){
     var max_sum = mcs([-1, 5, 6, -2, 20, -50, 4]);
-    assert.deepEqual(max_sum, [5,6,-2,20]);
+    assert.equal(max_sum, 29);
   });
 
 
   it('works in this scenario', function(){
     var  max_sum = mcs([10, -1, -2, -11, 1, 10]);
-    assert.deepEqual(max_sum, [1, 10]);
+    assert.equal(max_sum, 11);
   });
 
 
   it('works with all negatives and a zero', function(){
     var max_sum = mcs([-1, -20, 0]);
-    assert.deepEqual(max_sum, [0]);
+    assert.equal(max_sum, 0);
   });
 });
